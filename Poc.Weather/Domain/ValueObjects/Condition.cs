a1606be0eabedee26877e137d38c91732d0b27c5
Carlos Encine <carlos.encine@outlook.com>
2023-10-08 00:49:23 -0300

    feat: Add HttpClientService

---
+++ Poc.Weather/Domain/ValueObjects/Condition.cs
@@ -0,0 +1,6 @@
+﻿public class Condition
+{
+    public string text { get; set; }
+    public string icon { get; set; }
+    public int code { get; set; }
+}
---
+++ Poc.Weather/Infrastructure/Services/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poc.Weather.Infrastructure.Services
+{
+    public class HttpClientService
+    {
+        public IHttpClientFactory ClientFactory { get; set; }
+
+        public HttpClientService(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+
+        public async Task<T> Get<T>(string url)
+        {
+            var client = await ClientFactory.CreateClient().GetAsync(url);
+            return await client.Content.ReadAsAsync<T>();
+        }
+    }
+}
