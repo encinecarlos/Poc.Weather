e39a8539fad14b8412a1396d66f80fa2ea91d3ed
Carlos Encine <carlos.encine@outlook.com>
2023-10-21 09:26:38 -0300

    feat: remove configuration data

--- Poc.Weather/CurrentWeather.cs
+++ Poc.Weather/CurrentWeather.cs
@@ -1,16 +1,39 @@
 using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Poc.Weather.Domain.Entities;
+using Poc.Weather.Infrastructure.Services;
 
 namespace Poc.Weather
 {
     public class CurrentWeather
     {
-        [FunctionName("CurrentWeather")]
-        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly HttpClientService _httpClientService;
+        private readonly IConfiguration _configuration;
+
+        public CurrentWeather(HttpClientService http, IConfiguration configuration)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            _httpClientService = http;
+            _configuration = configuration;
+        }
+        [FunctionName("CurrentWeather")]
+        public async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            var url = $"{_configuration.GetSection("baseUrl").Value}?q={_configuration.GetSection("location").Value}&key={_configuration.GetSection("apiKey").Value}";
+            var result = await _httpClientService.Get<WeatherReponse>(url);
+            var response = new StringBuilder();
+            
+            response.AppendLine("Current Weather");
+            response.AppendLine($"location {result.Location.name}");
+            response.AppendLine($"temperature: {result.Current.temp_c}");
+            response.AppendLine($"condittion: {result.Current.condition.text}");
+            
+            log.LogInformation($"{response.ToString()}");
         }
     }
 }
